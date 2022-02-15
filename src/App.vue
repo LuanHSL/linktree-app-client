a19dd4a4b2f97ceee75b11813431405816b9b6ba
luansilvalemes <luansilvalemes@gmail.com>
2022-02-14 23:50:01 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <QRCode />
+  </header>
+</template>
+
+<script setup>
+import QRCode from './components/QRCode.vue';
+</script>
+
+<style>
+@import './assets/base.css';
+
+
+</style>
---
+++ src/components/QRCode.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <input type="text" size="25" placeholder="Digite para gerar..." v-model="text" />
+
+    <div v-if="text">
+      <img :src="newQRCode" alt="QRCode" />
+    </div>
+  </div>
+</template>
+
+<script>
+import QRious from 'qrious';
+
+export default {
+  name: 'QRCode', 
+  data() {
+    return {
+      title: 'Gerador de QR Code',
+      text: 'https://google.com.br/',
+      qrcode: new QRious({ size: 300 }),
+    };
+  },
+  computed: {
+    newQRCode() {
+      this.setQRCodeValue();
+      return this.qrcode.toDataURL();
+    },
+  },
+  methods: {
+    setQRCodeValue() {
+      this.qrcode.value = this.text;
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
